18017b8433d78e2f5db9876ba995e0f9026cbb5b
ViniciusPolachini <vipolachini@uol.com.br>
2019-11-30 13:09:03 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -15,39 +15,39 @@ const upload = multer(uploadConfig);
 
 //Session
 routes.post('/Session/create', Session.store);
-routes.get('/Session/login', Session.show);
+routes.post('/Session/login', Session.show);
 
 //ServicosProjetos
 routes.post('/ServicosProjetos/create', ServicosProjetos.store);
-routes.get('/ServicosProjetos/list', ServicosProjetos.index);
+routes.post('/ServicosProjetos/list', ServicosProjetos.index);
 routes.post('/ServicosProjetos/update', ServicosProjetos.update);
 routes.delete('/ServicosProjetos/delete', ServicosProjetos.destroy);
 
 //WhatsApp
 routes.post('/WhatsApp/register', WhatsApp.store);
-routes.get('/WhatsApp/show', WhatsApp.show);
+routes.post('/WhatsApp/show', WhatsApp.show);
 routes.post('/WhatsApp/update', WhatsApp.update);
 
 //Logo
 routes.post('/Logo/register', upload.single('thumbnail'), Logo.store);
 routes.post('/Logo/update', upload.single('thumbnail'), Logo.update);
-routes.get('/Logo/index', Logo.index);
+routes.post('/Logo/index', Logo.index);
 routes.delete('/Logo/delete', Logo.destroy);
 
 //FotoGestão
 routes.post('/Gestao/register', upload.single('thumbnail'), Gestao.store);
 routes.post('/Gestao/update', upload.single('thumbnail'), Gestao.update);
-routes.get('/Gestao/index', Gestao.index);
+routes.post('/Gestao/index', Gestao.index);
 routes.delete('Gestao/delete', Gestao.destroy);
 
 //Postagem
 routes.post('/Postagem/create', upload.single('thumbnail'), Postagem.store);
-routes.get('/Postagem/list', Postagem.index);
+routes.post('/Postagem/list', Postagem.index);
 routes.post('/Postagem/update', upload.single('thumbnail'), Postagem.update);
 routes.delete('/Postagem/delete', Postagem.destroy);
 
 //Contato
-routes.get('/Email/send', Contato.send);
+routes.post('/Email/send', Contato.send);
 
 
 module.exports = routes;
